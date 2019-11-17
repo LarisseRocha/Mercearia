892ff3cc12ebfa4da3050fc10659b96c89ef8d96
Larisse <larisse16rocha@gmail.com>
2019-11-17 19:14:59 -0300

    Projeto mercado

---
+++ src/br/edu/ifnmg/Mercearia/Fornecedor.java
@@ -0,0 +1,158 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.Mercearia;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Larisse
+ */
+public class Fornecedor {
+    private int cnpj;
+    private String razaoSocial;
+    private String email;
+    private String rua;
+    private int numero;
+    private String bairro;
+    private String cidade;
+    private String estado;
+
+    public Fornecedor(int cnpj, String razaoSocial, String email, String rua, int numero, String bairro, String cidade, String estado) {
+        this.cnpj = 0;
+        this.razaoSocial = "";
+        this.email = "";
+        this.rua = "";
+        this.numero = 0;
+        this.bairro = "";
+        this.cidade = "";
+        this.estado = "";
+    }
+
+    public int getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(int cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getRazaoSocial() {
+        return razaoSocial;
+    }
+
+    public void setRazaoSocial(String razaoSocial) {
+        this.razaoSocial = razaoSocial;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 31 * hash + this.cnpj;
+        hash = 31 * hash + Objects.hashCode(this.razaoSocial);
+        hash = 31 * hash + Objects.hashCode(this.email);
+        hash = 31 * hash + Objects.hashCode(this.rua);
+        hash = 31 * hash + this.numero;
+        hash = 31 * hash + Objects.hashCode(this.bairro);
+        hash = 31 * hash + Objects.hashCode(this.cidade);
+        hash = 31 * hash + Objects.hashCode(this.estado);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Fornecedor other = (Fornecedor) obj;
+        if (this.cnpj != other.cnpj) {
+            return false;
+        }
+        if (this.numero != other.numero) {
+            return false;
+        }
+        if (!Objects.equals(this.razaoSocial, other.razaoSocial)) {
+            return false;
+        }
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.rua, other.rua)) {
+            return false;
+        }
+        if (!Objects.equals(this.bairro, other.bairro)) {
+            return false;
+        }
+        if (!Objects.equals(this.cidade, other.cidade)) {
+            return false;
+        }
+        if (!Objects.equals(this.estado, other.estado)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Fornecedor{" + "cnpj=" + cnpj + ", razaoSocial=" + razaoSocial + ", email=" + email + ", rua=" + rua + ", numero=" + numero + ", bairro=" + bairro + ", cidade=" + cidade + ", estado=" + estado + '}';
+    }
+    
+    
+}
