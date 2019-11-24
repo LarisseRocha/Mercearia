bc0e020d8445e768f69b126951997346f5d47213
Larisse <larisse16rocha@gmail.com>
2019-11-24 18:06:21 -0200

    Conexão com o banco de dados

--- src/br/edu/ifnmg/Mercearia/Persistence/BancoDados.java
+++ src/br/edu/ifnmg/Mercearia/Persistence/BancoDados.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.edu.ifnmg.Persistence;
+package br.edu.ifnmg.Mercearia.Persistence;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
