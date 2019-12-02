d31442afc1130e2fe9a5be947aca81ca4ef429d8
Larisse <larisse16rocha@gmail.com>
2019-12-02 11:09:23 -0200

    Classe fornecedor

--- src/br/edu/ifnmg/Mercearia/DomainModel/Produto.java
+++ src/br/edu/ifnmg/Mercearia/DomainModel/Produto.java
@@ -31,7 +31,6 @@ public class Produto {
     }
 
     public Produto() {
-//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public int getId() {
