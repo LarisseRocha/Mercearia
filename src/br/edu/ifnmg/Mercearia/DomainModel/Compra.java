c9e344ce1091fa4119ce397a7f687036eb281c01
Larisse <larisse16rocha@gmail.com>
2019-12-02 11:13:44 -0200

    Registrar compra

--- src/br/edu/ifnmg/Mercearia/DomainModel/Compra.java
+++ src/br/edu/ifnmg/Mercearia/DomainModel/Compra.java
@@ -114,7 +114,7 @@ public class Compra {
     }
 
     public Object getProduto() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 
