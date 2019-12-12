45f45fed427842b3c32496e96aa537291c735b75
Larisse <larisse16rocha@gmail.com>
2019-12-12 20:31:38 -0200

    Compra

--- src/br/edu/ifnmg/Mercearia/DomainModel/Compra.java
+++ src/br/edu/ifnmg/Mercearia/DomainModel/Compra.java
@@ -6,6 +6,7 @@
 package br.edu.ifnmg.Mercearia.DomainModel;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 
@@ -16,16 +17,26 @@ import java.util.Objects;
 public class Compra {
      private int id; 
      private float valorTotal;
+     private int quantidade;
      Date dtVenda;
      private String operador;
      private Fornecedor fornecedor ;
+    public Produto produto;
 
-    public Compra(int id, float valorTotal, Date dtVenda,  Fornecedor fornecedor) {
+    public Compra(int id, float valorTotal, int quantidade, Date dtVenda, String operador, Fornecedor fornecedor, Produto produto) {
         this.id = 0;
         this.valorTotal = 0;
         this.dtVenda = dtVenda;
         this.operador = "";
         this.fornecedor = fornecedor;
+        this.quantidade = 0;
+        this.produto = produto;
+    }
+
+   
+
+    public Compra() {
+//        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public int getId() {
@@ -67,14 +78,32 @@ public class Compra {
         this.operador = operador;
     }
 
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
     @Override
     public int hashCode() {
-        int hash = 7;
-        hash = 53 * hash + this.id;
-        hash = 53 * hash + Float.floatToIntBits(this.valorTotal);
-        hash = 53 * hash + Objects.hashCode(this.dtVenda);
-        hash = 53 * hash + Objects.hashCode(this.operador);
-        hash = 53 * hash + Objects.hashCode(this.fornecedor);
+        int hash = 5;
+        hash = 41 * hash + this.id;
+        hash = 41 * hash + Float.floatToIntBits(this.valorTotal);
+        hash = 41 * hash + this.quantidade;
+        hash = 41 * hash + Objects.hashCode(this.dtVenda);
+        hash = 41 * hash + Objects.hashCode(this.operador);
+        hash = 41 * hash + Objects.hashCode(this.fornecedor);
+        hash = 41 * hash + Objects.hashCode(this.produto);
         return hash;
     }
 
@@ -96,6 +125,9 @@ public class Compra {
         if (Float.floatToIntBits(this.valorTotal) != Float.floatToIntBits(other.valorTotal)) {
             return false;
         }
+        if (this.quantidade != other.quantidade) {
+            return false;
+        }
         if (!Objects.equals(this.operador, other.operador)) {
             return false;
         }
@@ -105,22 +137,24 @@ public class Compra {
         if (!Objects.equals(this.fornecedor, other.fornecedor)) {
             return false;
         }
+        if (!Objects.equals(this.produto, other.produto)) {
+            return false;
+        }
         return true;
     }
 
     @Override
     public String toString() {
-        return "Compra{" + "id=" + id + ", valorTotal=" + valorTotal + ", dtVenda=" + dtVenda + ", operador=" + operador + ", fornecedor=" + fornecedor + '}';
+        return "Compra{" + "id=" + id + ", valorTotal=" + valorTotal + ", quantidade=" + quantidade + ", dtVenda=" + dtVenda + ", operador=" + operador + ", fornecedor=" + fornecedor + ", produto=" + produto + '}';
     }
 
-    public Object getProduto() {
-         return null;
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public List<Produto> getProdutos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
 
-   
-
-   
+    public void addProduto(Produto produto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+     
 
 }
