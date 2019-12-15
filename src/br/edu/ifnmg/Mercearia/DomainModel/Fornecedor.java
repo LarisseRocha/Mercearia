726f0bb8f9766ab1270459181beb892a884a2e7b
Larisse <larisse16rocha@gmail.com>
2019-12-15 15:07:26 -0200

    Registrar fornecedor

--- src/br/edu/ifnmg/Mercearia/DomainModel/Fornecedor.java
+++ src/br/edu/ifnmg/Mercearia/DomainModel/Fornecedor.java
@@ -267,9 +267,6 @@ public class Fornecedor {
         return razaoSocial;
     }
     
-
-    
-
     public void setEstado(String mg) {
         //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
