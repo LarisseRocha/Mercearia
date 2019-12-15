242905d3eaa980d90b0a9b4a468b1dee339b02cc
Larisse <larisse16rocha@gmail.com>
2019-12-15 15:05:54 -0200

    Multiplicar valor total

--- src/br/edu/ifnmg/Mercearia/DomainModel/Compra.java
+++ src/br/edu/ifnmg/Mercearia/DomainModel/Compra.java
@@ -5,6 +5,7 @@
  */
 package br.edu.ifnmg.Mercearia.DomainModel;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
@@ -18,24 +19,30 @@ public class Compra {
      private int id; 
      private float valorTotal;
      private int quantidade;
-     Date dtVenda;
-     private String operador;
+     //Date dtVenda;
+    // private String operador;
      private Fornecedor fornecedor ;
-    public Produto produto;
+     public List<Produto> produtos;
 
     public Compra(int id, float valorTotal, int quantidade, Date dtVenda, String operador, Fornecedor fornecedor, Produto produto) {
         this.id = 0;
         this.valorTotal = 0;
-        this.dtVenda = dtVenda;
-        this.operador = "";
+       // this.dtVenda = dtVenda;
+        //this.operador = "";
         this.fornecedor = fornecedor;
         this.quantidade = 0;
-        this.produto = produto;
+        this.produtos = new ArrayList<Produto>();
     }
 
    
 
     public Compra() {
+        this.produtos = new ArrayList<Produto>();
+        this.id = 0;
+        this.valorTotal = 0;
+       // this.dtVenda = dtVenda;
+        //this.operador = "";
+        this.quantidade = 0;
 //        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
@@ -55,13 +62,13 @@ public class Compra {
         this.valorTotal = valorTotal;
     }
 
-    public Date getDtVenda() {
+   /* public Date getDtVenda() {
         return dtVenda;
     }
 
     public void setDtVenda(Date dtVenda) {
         this.dtVenda = dtVenda;
-    }
+    }*/
     public Fornecedor getFornecedor() {
         return fornecedor;
     }
@@ -70,13 +77,13 @@ public class Compra {
         this.fornecedor = fornecedor;
     }
 
-    public String getOperador() {
+   /* public String getOperador() {
          return operador;
     }
 
     public void setOperador(String operador) {
         this.operador = operador;
-    }
+    }*/
 
     public int getQuantidade() {
         return quantidade;
@@ -86,74 +93,28 @@ public class Compra {
         this.quantidade = quantidade;
     }
 
-    public Produto getProduto() {
-        return produto;
-    }
 
-    public void setProduto(Produto produto) {
-        this.produto = produto;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 41 * hash + this.id;
-        hash = 41 * hash + Float.floatToIntBits(this.valorTotal);
-        hash = 41 * hash + this.quantidade;
-        hash = 41 * hash + Objects.hashCode(this.dtVenda);
-        hash = 41 * hash + Objects.hashCode(this.operador);
-        hash = 41 * hash + Objects.hashCode(this.fornecedor);
-        hash = 41 * hash + Objects.hashCode(this.produto);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Compra other = (Compra) obj;
-        if (this.id != other.id) {
-            return false;
-        }
-        if (Float.floatToIntBits(this.valorTotal) != Float.floatToIntBits(other.valorTotal)) {
-            return false;
-        }
-        if (this.quantidade != other.quantidade) {
-            return false;
-        }
-        if (!Objects.equals(this.operador, other.operador)) {
-            return false;
-        }
-        if (!Objects.equals(this.dtVenda, other.dtVenda)) {
-            return false;
-        }
-        if (!Objects.equals(this.fornecedor, other.fornecedor)) {
-            return false;
-        }
-        if (!Objects.equals(this.produto, other.produto)) {
-            return false;
-        }
-        return true;
-    }
+ 
 
     @Override
     public String toString() {
-        return "Compra{" + "id=" + id + ", valorTotal=" + valorTotal + ", quantidade=" + quantidade + ", dtVenda=" + dtVenda + ", operador=" + operador + ", fornecedor=" + fornecedor + ", produto=" + produto + '}';
+        return "Compra{" + "id=" + id + ", valorTotal=" + valorTotal + ", quantidade=" + quantidade + ", fornecedor=" + fornecedor + ", produtos=" + produtos + '}';
     }
 
+   
+
+
+
+    
     public List<Produto> getProdutos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         return this.produtos;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void addProduto(Produto produto) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void addProduto(Produto produto, int quantidade) {
+        this.produtos.add(produto);
+        this.valorTotal+= produto.getPrCompra()*quantidade;
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
      
 
